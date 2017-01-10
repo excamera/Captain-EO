f5d1f9ea4658429c00d5d6085da1645d63c3520b
John Emmons <emmons.john@gmail.com>
2017-01-10 13:29:39 -0800

    wrote new barcoder interface (commented out old interface for now)

--- src/barcoder/barcoder.hh
+++ src/barcoder/barcoder.hh
@@ -3,30 +3,35 @@
 #include <array>
 #include "display.hh"
 
-
 class Barcoder {
 public:
-    Barcoder(std::ifstream& rawfile, int width, int height, int codesize, int nbitsPerRow);
-    Barcoder(std::ifstream& rawfile);
-    ~Barcoder(); 
+    // Barcoder(std::ifstream& rawfile, int width, int height, int codesize, int nbitsPerRow);
+    // Barcoder(std::ifstream& rawfile);
+    // ~Barcoder(); 
 
-    void barcodeFrame(RGBPixel *frame_bytes); 
-    friend std::ostream& operator<<(std::ostream& os, Barcoder& br);
+    static void applyBarcode(XImage& image, int barcode_num); 
+    static int readBarcode(XImage& image);
+    
+    //void barcodeFrame(RGBPixel *frame_bytes); 
+    // friend std::ostream& operator<<(std::ostream& os, Barcoder& br);
 
 private:
-    void barcodeFrameLower(RGBPixel *frame_bytes, uint64_t frameno);
-    void barcodeFrameUpper(RGBPixel *frame_bytes, uint64_t frameno);
-    void writeBit(RGBPixel* frame_bytes, int x, int y, bool set);
+    static void applyBarcode(XImage& image, int barcode_num, int x_offset, int y_offset); 
+    static int readBarcode(XImage& image, int x_offset, int y_offset); 
 
-    int m_height;
-    int m_width;
-    int m_bytes_per_row;
-    int m_total_bits;
-    int m_code_size;
-    int m_rowBits;
+    //void barcodeFrameLower(RGBPixel *frame_bytes, uint64_t frameno);
+    //void barcodeFrameUpper(RGBPixel *frame_bytes, uint64_t frameno);
+    // void writeBit(RGBPixel* frame_bytes, int x, int y, bool set);
 
-    uint64_t m_next_frameno;
-    std::ifstream& m_input;
+    // int m_height;
+    // int m_width;
+    // int m_bytes_per_row;
+    // int m_total_bits;
+    // int m_code_size;
+    // int m_rowBits;
+
+    // uint64_t m_next_frameno;
+    // std::ifstream& m_input;
 };
 
-std::ostream& operator<<(std::ostream& os, Barcoder& br);
+// std::ostream& operator<<(std::ostream& os, Barcoder& br);
