65c67661fc10925d9a52fe33c7014c6afee9c6db
John Emmons <emmons.john@gmail.com>
2017-01-10 19:59:25 -0800

    barcode are placed in upper right and lower right of frames

--- src/barcoder/barcoder.cpp
+++ src/barcoder/barcoder.cpp
@@ -3,10 +3,19 @@
 static RGBPixel White = {0xFF, 0xFF, 0xFF, 0x0};
 static RGBPixel Black = {0x0, 0x0, 0x0, 0x0};
 static int barcode_grid_size = 4; // elements in each row and column
-static int barcode_edge_len = 10; // height and width in pixels of each element
+static int barcode_edge_len = 10; // height and width of each element (in pixels)
 
-void Barcoder::applyBarcode(XImage& image, uint16_t barcode_num){
-    applyBarcode(image, barcode_num, image.width()/2, image.height()/2);
+void Barcoder::applyBarcode(XImage& image, uint16_t barcode_num)
+{
+    // apply upper left (UL) barcode
+    applyBarcode(image, barcode_num,
+                 0,
+                 0);
+
+    // apply lower right (LR) barcode
+    applyBarcode(image, barcode_num,
+                 image.width() - barcode_grid_size*barcode_edge_len,
+                 image.height() - barcode_grid_size*barcode_edge_len);
 }
 
 void Barcoder::applyBarcode(XImage& image, uint16_t barcode_num, int xpos, int ypos){
@@ -40,69 +49,3 @@ uint16_t Barcoder::readBarcode(XImage& image, int x_offset, int y_offset){
     image.data();
     return 42;
 }
-
-// Barcoder::Barcoder(std::ifstream& rawfile, 
-//                     int width, int height, 
-//                     int codesize, int nbitsPerRow) : 
-//     m_height(height), 
-//     m_width(width),
-//     m_bytes_per_row(width * 4),
-//     m_total_bits(nbitsPerRow * nbitsPerRow),
-//     m_code_size(codesize),
-//     m_rowBits(nbitsPerRow),
-//     m_next_frameno(0),
-//     m_input(rawfile)
-// {}
-
-// Barcoder::Barcoder(std::ifstream& rawfile) :
-//     m_height(720), 
-//     m_width(1280),
-//     m_bytes_per_row(1280 * 4),
-//     m_total_bits(4 * 4),
-//     m_code_size(16),
-//     m_rowBits(4),
-//     m_next_frameno(0),
-//     m_input(rawfile)
-// {}
-
-// Barcoder::~Barcoder() {}
-
-// void Barcoder::barcodeFrameUpper(RGBPixel *frame_bytes, uint64_t frameno) {
-//     for (int i = 0; i < m_total_bits; i++) 
-//         writeBit(frame_bytes, 
-//                  (i % m_rowBits) * m_code_size, 
-//                  (i / m_rowBits) * m_code_size, 
-//                  frameno & (1 << i));
-// }
-// void Barcoder::barcodeFrameLower(RGBPixel *frame_bytes, uint64_t frameno) {
-//     for (int i = 0; i < m_total_bits; i++) 
-//         writeBit(frame_bytes, 
-//                  (m_width - (m_code_size * m_rowBits)) + (i % m_rowBits) * m_code_size, 
-//                  (m_height - (m_code_size * m_rowBits)) + (i / m_rowBits) * m_code_size, 
-//                  frameno & (1 << i));
-// }
-
-// void Barcoder::barcodeFrame(RGBPixel *frame_bytes) {
-//     barcodeFrameUpper(frame_bytes, m_next_frameno);
-//     barcodeFrameLower(frame_bytes, m_next_frameno);
-//     m_next_frameno++;
-// }
-
-// void Barcoder::writeBit(RGBPixel* frame_bytes, int x, int y, bool set) {
-//     for (int i = y; i < y + code_width; i++) 
-//         for (int j = x; j < x + code_height; j++)
-//             frame_bytes[i * m_width + j] = set ? Black : White;
-// }
-
-// std::ostream& operator<<(std::ostream& os, Barcoder& br)   {
-//     const uint64_t frame_size = br.m_height * br.m_bytes_per_row;
-//     std::vector<char> buf_vector( frame_size );
-//     char *buf = buf_vector.data();
-//     while (true) {
-//         if (br.m_input.read(buf, frame_size).eof())
-//             break;
-//         br.barcodeFrame((RGBPixel*)buf);
-//         os.write(buf, frame_size);
-//     }
-//     return os;
-// }
