27dcbf7c555aaddd4bbb5f11486cbebbbd468b3d
Keith Winstein <keithw@cs.stanford.edu>
2017-01-17 21:38:13 -0800

    Add script to generate a "good/bad" binary cellsim/mm-link trace

---
+++ scripts/good-bad-tracegen.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $timestamp = 0.0;
+
+sub Mbps_to_ppms ($) {
+  my ( $Mbps ) = @_;
+  my $packets_per_ms = 1e6 * $Mbps / (1500 * 8 * 1e3);
+  print STDERR "megabits per second of $Mbps => packets per ms of $packets_per_ms\n";
+  return $packets_per_ms;
+}
+
+my $good_Mbps = 2;
+my $good_packets_per_millisecond = Mbps_to_ppms $good_Mbps;
+
+while ( $timestamp <= 10e3 ) {
+  print int( $timestamp ), "\n";
+  $timestamp += 1 / $good_packets_per_millisecond;
+}
+
+my $bad_Mbps = 0.2;
+my $bad_packets_per_millisecond = Mbps_to_ppms $bad_Mbps;
+
+while ( $timestamp <= 20e3 ) {
+  print int( $timestamp ), "\n";
+  $timestamp += 1 / $bad_packets_per_millisecond;
+}
