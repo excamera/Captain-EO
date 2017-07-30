447df41aa130e77f5b83d5a0b5e138b12557b1d3
John Emmons <emmons.john@gmail.com>
2017-07-30 12:31:27 -0700

    Fix a bug that caused double writing

--- scripts/show.sh
+++ scripts/show.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# exec mplayer $1 -demuxer rawvideo -rawvideo h=720:w=1280:format=bgra:fps=59.94
-exec mplayer $1 -demuxer rawvideo -rawvideo h=1080:w=1920:format=bgra:fps=59.94
+exec mplayer $1 -demuxer rawvideo -rawvideo h=720:w=1280:format=bgra:fps=59.94
+# exec mplayer $1 -demuxer rawvideo -rawvideo h=1080:w=1920:format=bgra:fps=59.94
